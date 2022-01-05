a68a76758e45690eada63d36b8b52a0c73263e42
Michele Albrigo <michele.albrigo@univr.it>
2022-01-05 15:47:19 +0100

    Enabled error reporting

--- token.php
+++ token.php
@@ -6,6 +6,11 @@
   * v0.0 - initial structure
  */
 
+// enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /**
   * Defaults setting
  */
