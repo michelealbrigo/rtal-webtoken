681155329ad0753db48d7865ad72031f5dec0eba
Michele Albrigo <michele.albrigo@univr.it>
2022-01-17 16:34:40 +0100

    Base64 encoding of Token

--- token.php
+++ token.php
@@ -218,9 +218,9 @@ if ($service == 'token_generation') {
     $privkey = openssl_pkey_get_private(file_get_contents($private_key_file));
     $pubkey = openssl_pkey_get_public(file_get_contents($public_key_file));
     $original = $system_seed.":".$opcode.":".$username.":"."timestamp";
-    openssl_private_encrypt($original, $token, $privkey);
+    openssl_private_encrypt($original, $bintoken, $privkey);
     echo "<table>
-    <tr><td><pre>".openssl_pkey_get_details($pubkey)['key']."</pre></td><td>TOKEN:".$token."</td><td>Original:".$original."</td></tr>
+    <tr><td><pre>".openssl_pkey_get_details($pubkey)['key']."</pre></td><td>TOKEN:".base64_encode($token)."</td><td>Original:".$original."</td></tr>
     </table>
     ";
 
