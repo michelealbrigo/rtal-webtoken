87fcbcccf5f154ce19f45c4d5b6e39f73b0ad4bb
Michele Albrigo <michele.albrigo@univr.it>
2022-01-21 14:20:04 +0100

    Fixing decryption presentation

--- token.php
+++ token.php
@@ -307,7 +307,7 @@ if ( $reqservice == 'token_decryption' ) {
     <tr><td><b>Encrypted:</b></td><td>'.$reqtoken.'</td></tr>
     <tr><td><b>Decrypted:</b></td><td>'.$dectoken.'</td></tr>
     <tr><td><b>Server ID:</b></td><td>'.substr($dectoken,0,10).'</td></tr>
-    <tr><td><b>Opcode:</b></td><td>'.substr($dectoken,10,10).'</td></tr>
+    <tr><td><b>Opcode:</b></td><td>'.substr($dectoken,11,10).'</td></tr>
     <tr><td><b>Timestamp:</b></td>'.substr($dectoken,22,10).'<td></td></tr>
     <tr><td><b>Username:</b></td>'.substr($dectoken,33).'<td></td></tr>
 
